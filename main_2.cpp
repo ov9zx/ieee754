b2ca26383f9fb49b7a536ec8762e0bdc28dcadb5
rick <kadhimmustafa405@gmail.com>
2025-12-18 10:03:39 +0300

    Update main_2.cpp

--- main_2.cpp
+++ main_2.cpp
@@ -3,19 +3,31 @@
 using namespace std;
 
 int main() {
-    unsigned int ieee ;
-    cout<<"ieee:";
-    cin>>ieee;
+    unsigned int ieee;
+    cout << "ieee: ";
+    cin >> ieee;
 
-    int S = (ieee >> 31) ;
-    int E = (ieee >> 23) ;
-    unsigned int M = ieee & ((1 << 23) - 1);
+    unsigned int S = (ieee >> 31);
+    unsigned int E = (ieee >> 23) & 255;
+    unsigned int M = ieee & 8388607;
+    float result;
+    if (E == 0) {
 
-    double result = pow(-1, S) * pow(2, E - 127) * (1.0 + M / pow(2,23));
+        result = pow(-1, S) * pow(2, -126) * (M / 8388608.0);
+    } else {
 
+        result = pow(-1, S) * pow(2, E - 127) * (1.0 + M / 8388608.0);
+    }
+
+    cout << "S: " << S << " E: " << E << " M: " << M << endl;
     cout << "IEEE 754: " << ieee << " -> " << result << endl;
-    cout << "M: " << M << endl;
 
     return 0;
 }
-//1125883904
+
+//1125883904_155.625
+
+
+//3273367552_-155.625
+
+//
